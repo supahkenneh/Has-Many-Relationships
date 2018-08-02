54bde51f7eab9e1369fa67b600cca4ab8f9b7187
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-02 10:01:23 -1000

    reformating

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -3,31 +3,34 @@ DROP DATABASE IF EXISTS has_many_blogs;
 CREATE USER has_many_user;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 
+\c has_many_blogs;
+
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
   users_id          serial                    NOT NULL  PRIMARY KEY,
   username          varchar(90)               NOT NULL,
-  first_name        varchar(90)               NULL      DEFAULT NULL,
-  last_name         varchar(90)               NULL      DEFAULT NULL,
+  first_name        varchar(90)                         DEFAULT NULL,
+  last_name         varchar(90)                         DEFAULT NULL,
   user_created_at   timestamp with time zone  NOT NULL  DEFAULT now(),
   user_updated_at   timestamp with time zone  NOT NULL  DEFAULT now()
 );
 
-DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
   post_id            serial                    NOT NULL  PRIMARY KEY,
-  post_title         varchar(180)              NULL      DEFAULT NULL,
-  post_url           varchar(510)              NULL      DEFAULT NULL,
-  post_content       text                      NULL      DEFAULT NULL,
+  post_title         varchar(180)                        DEFAULT NULL,
+  post_url           varchar(510)                        DEFAULT NULL,
+  post_content       text                                DEFAULT NULL,
   post_created_at    timestamp with time zone  NOT NULL  DEFAULT now(),
   post_updated_at    timestamp with time zone  NOT NULL  DEFAULT now(),
   users_id           integer                   NOT NULL  REFERENCES users (users_id)
 );
 
-DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
   comment_id       serial                    NOT NULL  PRIMARY KEY,
-  comment_body     varchar(510)              NULL      DEFAULT NULL,
+  comment_body     varchar(510)                        DEFAULT NULL,
   comm_created_at  timestamp with time zone  NOT NULL  DEFAULT now(),
   comm_updated_at  timestamp with time zone  NOT NULL  DEFAULT now(),
   post_id          integer                   NOT NULL  REFERENCES posts (post_id),
--- joins.sql
+++ joins.sql
@@ -5,58 +5,109 @@ SELECT * FROM users;
 SELECT * FROM posts WHERE users_id = 100;
 
 --3
-SELECT *, users.first_name, users.last_name
-FROM posts JOIN users ON posts.users_id = users.users_id
+SELECT 
+  p.*, 
+  users.first_name, 
+  users.last_name
+FROM posts AS p JOIN users ON p.users_id = users.users_id
 WHERE users.users_id = 200;
 
 --4
-SELECT username, last_name, posts.*
+SELECT 
+  username, 
+  last_name, 
+  posts.*
 FROM users JOIN posts ON posts.users_id = users.users_id
 WHERE first_name = 'Norene' AND last_name = 'Schmitt';
 
 --5
 SELECT username
-FROM users JOIN posts ON (posts.users_id = users.users_id)
+FROM users 
+JOIN posts ON (posts.users_id = users.users_id)
 WHERE posts.post_created_at > '2015-01-01 00:00:00-10';
 
 --6
-SELECT post_title, post_content, users.username
-FROM posts JOIN users ON (posts.users_id = users.users_id)
+SELECT 
+  post_title, 
+  post_content, 
+  users.username
+FROM posts 
+JOIN users ON (posts.users_id = users.users_id)
 WHERE users.user_created_at < '2015-01-01 00:00:00-10';
 
 --7
-SELECT posts.post_title AS "Post Title", * 
-FROM comments JOIN posts ON (comments.post_id = posts.post_id);
+SELECT 
+  posts.post_title AS "Post Title",
+  c.* 
+FROM comments AS c
+JOIN posts ON (c.post_id = posts.post_id);
 
 --8
-SELECT posts.post_title AS "post_title", posts.post_url AS "post_url", comments.comment_body AS "comment_body", *
-FROM comments JOIN posts ON (comments.post_id = posts.post_id)
+SELECT 
+  posts.post_title AS "post_title", 
+  posts.post_url AS "post_url", 
+  c.comment_body AS "comment_body", 
+  c.*
+FROM comments AS c
+JOIN posts ON (c.post_id = posts.post_id)
 WHERE posts.post_created_at < '2015-01-01 00:00:00-10';
 
 --9
-SELECT posts.post_title AS "post_title", posts.post_url AS "post_url", comments.comment_body AS "comment_body", *
-FROM comments JOIN posts ON (comments.post_id = posts.post_id)
+SELECT 
+  posts.post_title AS "post_title", 
+  posts.post_url AS "post_url", 
+  c.comment_body AS "comment_body", 
+  *
+FROM comments AS c
+JOIN posts ON (c.post_id = posts.post_id)
 WHERE posts.post_created_at > '2015-01-01 00:00:00-10';
 
 --10
-SELECT posts.post_title AS "post_title", posts.post_url AS "post_url", comments.comment_body AS "comment_body", *
-FROM comments JOIN posts ON (comments.post_id = posts.post_id)
-WHERE comments.comment_body LIKE '%USB%';
+SELECT 
+  posts.post_title AS "post_title", 
+  posts.post_url AS "post_url", 
+  c.comment_body AS "comment_body", 
+  c.*
+FROM comments AS c
+JOIN posts ON (c.post_id = posts.post_id)
+WHERE c.comment_body LIKE '%USB%';
 
 --11
-SELECT posts.post_title AS "post_title", users.first_name, users.last_name, comments.comment_body AS "comment_body"
-FROM comments JOIN users ON (comments.users_id = users.users_id) JOIN posts ON (comments.post_id = posts.post_id)
+SELECT 
+  posts.post_title AS "post_title", 
+  users.first_name, 
+  users.last_name, 
+  comments.comment_body AS "comment_body"
+FROM comments 
+JOIN users ON (comments.users_id = users.users_id) 
+JOIN posts ON (comments.post_id = posts.post_id)
 WHERE comments.comment_body LIKE '%matrix%';
 
 --12
-SELECT users.first_name, users.last_name, comment_body AS "comment_body"
-FROM comments JOIN users ON (comments.users_id = users.users_id) JOIN posts ON (comments.post_id = posts.post_id)
+SELECT 
+  users.first_name, 
+  users.last_name, 
+  comment_body AS "comment_body"
+FROM comments 
+JOIN users ON (comments.users_id = users.users_id) 
+JOIN posts ON (comments.post_id = posts.post_id)
 WHERE comment_body LIKE '%SSL%' AND posts.post_content LIKE '%dolorum%';
 
 --13
-SELECT users.first_name AS "post_author_first_name", users.last_name AS "post_author_last_name", posts.post_title AS "post_title", users.username AS "comment_author_username", comment_body AS "comment_body"
-FROM comments JOIN users ON (comments.users_id = users.users_id) JOIN posts ON (comments.post_id = posts.post_id)
-WHERE (comment_body LIKE '%SSL%' OR comment_body LIKE '%firewall%') AND posts.post_content LIKE '%nemo%';
+SELECT 
+  users.first_name AS "post_author_first_name", 
+  users.last_name AS "post_author_last_name", 
+  posts.post_title AS "post_title", 
+  users.username AS "comment_author_username", 
+  comment_body AS "comment_body"
+FROM comments 
+JOIN users ON (comments.users_id = users.users_id) 
+JOIN posts ON (comments.post_id = posts.post_id)
+WHERE (
+  comment_body LIKE '%SSL%' 
+  OR comment_body LIKE '%firewall%'
+) 
+AND posts.post_content LIKE '%nemo%';
 
 --14
 SELECT COUNT(*)
